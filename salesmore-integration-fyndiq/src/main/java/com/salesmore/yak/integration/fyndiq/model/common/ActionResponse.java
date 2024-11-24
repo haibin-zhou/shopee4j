8800adff4af366e2173940c6fa19aafcbb212534
283595276@qq.com <283595276@qq.com>
2024-11-24 18:25:37 +0800

    init pom

--- salesmore-integration-fyndiq/src/main/java/com/salesmore/yak/integration/fyndiq/model/common/ActionResponse.java
+++
@@ -1,8 +0,0 @@
-package com.salesmore.yak.integration.fyndiq.model.common;
-
-import com.salesmore.yak.integration.core.model.ModelEntity;
-
-public interface ActionResponse extends ModelEntity {
-
-    String getDescription();
-}
